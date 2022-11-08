0f3a80fef13badef5984b51c8b123157f625ff60
Ben Hessel <hesselfitness@gmail.com>
2022-11-08 18:17:43 -0500

    categories show

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,20 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styles: [],
 })
-
 export class HomeComponent implements OnInit {
+  cols = 3;
+  category: string | undefined;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
 }

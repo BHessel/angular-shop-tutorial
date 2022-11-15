6a463c8223681df65da59884edddefe27fd183db
Ben Hessel <hesselfitness@gmail.com>
2022-11-15 14:09:39 -0500

    all good through 205:50

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Product } from "src/app/models/product.model";
 
 @Component({
   selector: "app-product-box",
@@ -6,8 +7,22 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: "Sneakers",
+    price: 150,
+    category: "Shoes",
+    description: "lorem ipsum",
+    image: "https://via.placeholder.com/150",
+  };
+
+  @Output() addToCart = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -22,4 +22,8 @@ export class HomeComponent implements OnInit {
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
   }
+
+  onAddToCart(): void {
+    
+  }
 }
